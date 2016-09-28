1b290b5eb8a72e9f7a28db82224060bc1e560eb2
Manikant Upadhyay <manikant.upadhyay@magicsw.com>
2016-09-28 17:47:34 +0530

    adding file

---
+++ JS Training/assignment/js/activity.js
@@ -0,0 +1,19 @@
+var activity = (function () {
+	
+	var alphabets =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+	return{
+		createAlphabetSpan :function(){
+		for(var i=0;i<alphabets.length;i++)	{
+			console.log(alphabets[i]);
+            var main = $('#mainDiv');
+			var elm = '<div class="alphabet">' + alphabets[i] +
+	          '</div>';
+           $(elm).appendTo(main);
+		}
+		
+    },
+}
+})();
+
+activity.createAlphabetSpan();
